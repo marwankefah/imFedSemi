072c98a9c8b8bfe9b7f99bdf9f297060c1f2508b
marwan <marwankefah@gmail.com>
2022-07-09 04:34:02 +0200

    updated options with FixMatch

--- options.py
+++ options.py
@@ -22,5 +22,11 @@ def args_parser():
      parser.add_argument('--hi_lp', type=float,  default=0.9, help='confidence for label proportion estimation degree')
      parser.add_argument('--lo_lp', type=float,  default=0.5, help='confidence for label proportion estimation degree')
      parser.add_argument('--label_uncertainty', type=str,  default='U-Ones', help='label type')
+     parser.add_argument('--lambda-u', default=1, type=float,
+                         help='coefficient of unlabeled loss')
+     parser.add_argument('--threshold', default=0.95, type=float,
+                         help='pseudo label threshold')
+     parser.add_argument('--T', default=1, type=float,
+                         help='pseudo label temperature')
      args = parser.parse_args()
      return args
